4f338d2814190c8faff5fc2dab59a2a70c7e1745
rico.carbonel@motivit.com <rico.carbonel@motivit.com>
2026-02-27 05:39:12 +0800

    add patch in tasks

--- modules/tasks/task.router.js
+++ modules/tasks/task.router.js
@@ -6,22 +6,7 @@ const router = express.Router()
 
 router.get('/', taskController.getTasks)
 router.post('/', taskController.createTask)
-// router.patch('/:id', taskController.patchTask)
-// router.patch('/:id', (req, res) => {
-//   console.log('Direct test PATCH hit')
-//   console.log('Route param id:', req.params.id)
-//   console.log('Body:', req.body)
-
-//   res.json({
-//     ok: true,
-//     status: 200,
-//     message: 'Direct test PATCH successful',
-//     data: {
-//       id: req.params.id,
-//       ...req.body
-//     }
-//   })
-// })
+router.patch('/:id', taskController.patchTask)
 router.patch('/test', (req, res) => {
   console.log('PATCH test hit!', req.body)
   res.json({ ok: true, data: req.body })
